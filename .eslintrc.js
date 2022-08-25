12bf56cde43a32be6bf800fe1076de7dab01491f
Jared Arroyo <jararrmar@gmail.com>
2022-08-25 22:58:01 +0200

    Added lint on save

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,25 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+
   extends: [
     "plugin:vue/essential",
     "eslint:recommended",
@@ -11,18 +28,4 @@ module.exports = {
     //"@vue/prettier",
     //"@vue/prettier/@typescript-eslint",
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    //'@typescript-eslint/no-explicit-any': 'off',
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-  },
 };
